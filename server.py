62189e1ae62e46c4105072da6c632d3a7626310e
adam <a.daman.loo@gmail.com>
2017-10-04 02:44:55 -0400

    server python file created

---
+++ server.py
@@ -0,0 +1,21 @@
+####################################################
+#
+#   File        : server.py
+#   Project     : CMPEN Web Server and Client
+#   Goal        : Server process
+#   Description : Will use the relevent python
+#				  libraries to implement a web
+#				  server that displays a simple 
+#				  index page in HTML. 
+#
+#   Author      : Adam Loo
+#   Created     : 04-10-2017
+#   Last Edited :
+#
+####################################################
+
+import socket as sok
+
+print(type(sok))
+
+print('wuddup cuz')
