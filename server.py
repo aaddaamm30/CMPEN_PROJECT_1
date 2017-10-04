45d34719124cdc4671c772b154c7458dba8a3f15
adam <a.daman.loo@gmail.com>
2017-10-04 02:53:51 -0400

    cleaning

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 ####################################################
 #
 #   File        : server.py
+#
+#   Author      : Adam Loo
+#   Created     : 04-10-2017
+#   Last Edited : Wed 04 Oct 2017 02:53:31 AM EDT
+#
 #   Project     : CMPEN Web Server and Client
 #   Goal        : Server process
 #   Description : Will use the relevent python
 #				  libraries to implement a web
 #				  server that displays a simple 
 #				  index page in HTML. 
-#
-#   Author      : Adam Loo
-#   Created     : 04-10-2017
-#   Last Edited : Wed 04 Oct 2017 02:48:50 AM EDT
 #
 ####################################################
 
